23f2a659bf6b5ffd94591f46d75672952ce1cf53
KENSH1N <k3nshi9@gmail.com>
2025-10-21 15:07:29 +0600

    update swagger, api versioning from uri, enable cors for frontend ui and update database environment and module that support typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      // entities: [Project],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: parseInt(config.get<string>('DB_PORT') ?? '5432', 10),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // only for development
+        // migrations:true // for production
+        // entities: [Project],
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,44 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Travel Assurance API') // Your API title
+    .setDescription('API documentation') // Optional description
+    .setVersion('1.0') // API version
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Swagger UI available at http://localhost:3001/api
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //strips out properties that not in dto
+      forbidNonWhitelisted: true, //throws error if extra properties sent
+      transform: true, // transforms payload to DTO instances
+    }),
+  );
+
+  // Enable CORS
+  app.enableCors({
+    origin: [
+      'http://localhost:3000', // your Next.js frontend
+    ],
+    credentials: true, // allow cookies/authorization headers
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  // Enable versioning via URI
+  app.enableVersioning({
+    type: VersioningType.URI, // will use /v1, /v2, etc.
+    defaultVersion: '1', // default if not specified
+  });
 }
 bootstrap();
